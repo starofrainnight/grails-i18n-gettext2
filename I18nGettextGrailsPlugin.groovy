723fe4542808da3ae3b59196b00c4b9501d27899
rabrab <rabrab@832c1c66-4827-0410-8465-ccb17913cd09>
2009-03-02 16:22:37 +0000

    Small fix to the jar generation. Omitted the i18ngettext path before.

--- I18nGettextGrailsPlugin.groovy
+++ I18nGettextGrailsPlugin.groovy
@@ -21,7 +21,7 @@ import org.codehaus.groovy.grails.commons.ApplicationHolder
 
 class I18nGettextGrailsPlugin {
     def observe = ['*']
-    def version = 0.82
+    def version = 0.83
 
     def author = "Rainer Brang, Backend-Server GmbH & Co. KG"
     def authorEmail = "grails@backend-server.de"
